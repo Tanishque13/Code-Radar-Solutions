a6730ef4eb83664141550368db5b69536aa5146d
Tanishque <tanishque1308@gmail.com>
2025-03-18 12:21:14 +0530

    Solution to Check if a String Contains a Substring

--- Check if a String Contains a Substring@@676c039d80e1714600f6764e/code.c
+++ Check if a String Contains a Substring@@676c039d80e1714600f6764e/code.c
@@ -3,7 +3,7 @@
 #include<string.h>
 int main(){
     char str1[100];
-    char str2[]
+    char str2[100];
     fgets(str1,sizeof(str1),stdin);
     fgets(str2,sizeof(str2),stdin);
     if(strstr(str1,str2)!=NULL){
