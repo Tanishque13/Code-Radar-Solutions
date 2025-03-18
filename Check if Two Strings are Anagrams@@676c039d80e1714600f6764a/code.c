0173dd401e6ed40f41a9952972514ee756a9e836
Tanishque <tanishque1308@gmail.com>
2025-03-18 11:11:38 +0530

    Solution to Check if Two Strings are Anagrams

---
+++ Check if Two Strings are Anagrams@@676c039d80e1714600f6764a/code.c
@@ -0,0 +1,22 @@
+// Your code here...
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str1[100];
+    char str2[100];
+    int count1=0;
+    int count2=0;
+    fgets(str1,sizeof(str1),stdin);
+    fgets(str2,sizeof(str2),stdin);
+    for(int i=0;str[i]!='\0';i++){
+        count1++:
+        for(int j=0;str[j]!='\0',j++){
+            count2++;
+            if(count1==count2){
+                printf("Yes");
+            }else{
+                printf("No");
+            }
+        }
+    }
+}
