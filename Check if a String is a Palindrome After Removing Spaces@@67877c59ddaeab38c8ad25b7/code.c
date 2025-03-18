fea51708da59847a5f6a2a487106768396396f5f
Tanishque <tanishque1308@gmail.com>
2025-03-19 01:06:44 +0530

    Solution to Check if a String is a Palindrome After Removing Spaces

--- Check if a String is a Palindrome After Removing Spaces@@67877c59ddaeab38c8ad25b7/code.c
+++ Check if a String is a Palindrome After Removing Spaces@@67877c59ddaeab38c8ad25b7/code.c
@@ -1,32 +1,41 @@
 // Your code here...
-#include<stdio.h>
-#include<string.h>
-int main(){
-    char str[100];
-    char newstr[100];
-    fgets(str,sizeof(str),stdin);
-    int length=strlen(str);
-    
-    
-    int j=0;
-    for(int i=0;str[i]!='\0';i++){
-        if(str[i]!=' '){
-            newstr[j++]=str[i];
+#include <stdio.h>
+#include <string.h>
 
+int main() {
+    char str[100], newstr[100];
+    fgets(str, sizeof(str), stdin);
+
+   
+    str[strcspn(str, "\n")] = '\0';
+
+    int j = 0;
+
+   
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] != ' ') {
+            newstr[j++] = str[i];
         }
     }
-    newstr[j]='\0';
-    int length1=strlen(newstr);
-    int ispal=1;
-    for(int i=0;i<length1/2;i++){
-        if(newstr[i]!=newstr[length1-1-i]){
-            ispal=0;
+    newstr[j] = '\0'; 
+
+    int length1 = strlen(newstr);
+    int ispal = 1;
+
+    
+    for (int i = 0; i < length1 / 2; i++) {
+        if (newstr[i] != newstr[length1 - 1 - i]) {
+            ispal = 0;
             break;
         }
     }
-    if(ispal){
-        printf("Yes");
-    }else{
-        printf("No");
+
+    
+    if (ispal) {
+        printf("Yes\n");
+    } else {
+        printf("No\n");
     }
+
+    return 0;
 }
