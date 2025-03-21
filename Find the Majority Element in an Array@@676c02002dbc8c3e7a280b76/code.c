730460cee901889e1a67c27a33259b29b6776e9f
Tanishque <tanishque1308@gmail.com>
2025-03-21 20:51:43 +0530

    Solution to Find the Majority Element in an Array

--- Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
+++ Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
@@ -1,38 +1,37 @@
-// Your code here...
-#include<stdio.h>
-int main(){
-    int n,i;
-    int freq[1000]={0};
-    scanf("%d",&n);
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int freq[1000] = {0};  // Frequency array initialized to 0
+    scanf("%d", &n);
     int arr[n];
-    for(i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+
+    // Read input and count frequency
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
         freq[arr[i]]++;
-
     }
-    int greater=arr[0];
 
-    for(i=0;i<n;i++){
-        if(freq[arr[i]]!=0){
-            if(freq[arr[i]]>=freq[greater]){
-                greater=arr[i];
-                
+    int greater = arr[0];  // Assume first number is the most frequent
+    int maxFreq = freq[arr[0]];  // Store its frequency
 
-
+    // Find the number with the highest frequency
+    for (i = 0; i < n; i++) {
+        if (freq[arr[i]] != 0) {  
+            if (freq[arr[i]] > maxFreq || 
+               (freq[arr[i]] == maxFreq && arr[i] > greater)) {  
+                greater = arr[i];  // Update to most frequent or larger number if tie
+                maxFreq = freq[arr[i]];  // Update max frequency
             }
-            //freq[arr[i]]=0;
-            
-
         }
     }
-    int maxFreq = freq[greater];  
 
+    // Check if the most frequent number appears more than once
     if (maxFreq > 1) {
         printf("%d", greater);
     } else {
         printf("-1");
     }
+
+    return 0;
 }
-    
-
-
