fa93184db44d0f6a5c309b0a6e39ba9c0b32bd44
Tanishque <tanishque1308@gmail.com>
2025-03-21 20:53:26 +0530

    Solution to Find the Majority Element in an Array

--- Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
+++ Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
@@ -14,20 +14,23 @@ int main() {
 
     int greater = arr[0];  // Assume first number is the most frequent
     int maxFreq = freq[arr[0]];  // Store its frequency
+    int countMax = 0;  // To track how many numbers have max frequency
 
     // Find the number with the highest frequency
     for (i = 0; i < n; i++) {
         if (freq[arr[i]] != 0) {  
-            if (freq[arr[i]] > maxFreq || 
-               (freq[arr[i]] == maxFreq && arr[i] > greater)) {  
-                greater = arr[i];  // Update to most frequent or larger number if tie
+            if (freq[arr[i]] > maxFreq) {  
+                greater = arr[i];  // Update most frequent number
                 maxFreq = freq[arr[i]];  // Update max frequency
+                countMax = 1; // Reset count since we found a higher frequency
+            } else if (freq[arr[i]] == maxFreq && arr[i] != greater) {  
+                countMax++; // Another number has the same max frequency
             }
         }
     }
 
-    // Check if the most frequent number appears more than once
-    if (maxFreq > 1) {
+    // Check if there is a unique most frequent number
+    if (maxFreq > 1 && countMax == 1) {
         printf("%d", greater);
     } else {
         printf("-1");
