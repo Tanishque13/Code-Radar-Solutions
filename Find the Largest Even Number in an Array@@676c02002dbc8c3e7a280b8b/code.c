cf15dd71af69fd166954a23ad91ef9f5350f7a72
Tanishque <tanishque1308@gmail.com>
2025-03-22 03:18:54 +0530

    Solution to Find the Largest Even Number in an Array

--- Find the Largest Even Number in an Array@@676c02002dbc8c3e7a280b8b/code.c
+++ Find the Largest Even Number in an Array@@676c02002dbc8c3e7a280b8b/code.c
@@ -1,33 +1,32 @@
-// Your code here...
-#include<stdio.h>
-int main(){
-int n,i,j;
-    scanf("%d",&n);
-    
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+    scanf("%d", &n);
     
     int arr[n];
-    for(i=0;i<n;i++){
-        scanf("%d",&arr[i]);
-
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    int iseven=1;
-    for(i=0;i<n-1;i++){
-        if(i%2==0){
-            for(j=0;j<n-1-i;j++){
-                if(arr[j]>arr[j+1]){
-                    int temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
-                    iseven=0;
-                }
+
+    // Bubble sort to sort the array
+    int isSorted = 1;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                isSorted = 0;  // Means a swap happened, array wasn't sorted
             }
         }
-    }if(iseven){
-        printf("%d",arr[n]);
+    }
 
-    }else{
+    if (isSorted) {
+        printf("%d", arr[n - 1]); // Last element in sorted array
+    } else {
         printf("-1");
     }
-    
+
+    return 0;
 }
-    
