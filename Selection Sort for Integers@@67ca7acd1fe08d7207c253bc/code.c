2adf914e4e494913efab0a7e60934ab410825a2a
Tanishque <tanishque1308@gmail.com>
2025-03-28 16:44:00 +0530

    Solution to Selection Sort for Integers

--- Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
+++ Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
@@ -1,5 +1,5 @@
 // Your code here...
-void selectionSort(arr[],n){
+void selectionSort(int arr[],int n){
     for (int i = 0; i < n - 1; i++) {  // Number of passes
         for (int j = 0; j < n - i - 1; j++) {  // Compare adjacent elements
             if (arr[j] > arr[j + 1]) {
