4cd423f49d7486035b93699f962bc95b2ba12fe6
Tanishque <tanishque1308@gmail.com>
2025-04-01 00:20:21 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -2,10 +2,14 @@
 int fibonacciSeries(int n){
     int x=0;
     int y=1;
-    int sum=x+y;
-    x=y;
-    y=sum;
-    return sum;
+    for(int i=0;i<n;i++){
+        int sum=x+y;
+        x=y;
+        y=sum;
+        return sum;
+
+    }
+    
     
     
     
