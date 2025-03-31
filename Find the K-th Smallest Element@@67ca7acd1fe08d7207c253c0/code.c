559570b10e100b09e2178fe151178aa8037f06a2
Tanishque <tanishque1308@gmail.com>
2025-04-01 01:06:45 +0530

    Solution to Find the K-th Smallest Element

---
+++ Find the K-th Smallest Element@@67ca7acd1fe08d7207c253c0/code.c
@@ -0,0 +1,18 @@
+// Your code here...
+int searchInRotatedArray(int arr[],int n,int k){
+    if(k>n){
+        return -1;
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+
+                
+            }
+        }
+    }
+    return arr[k-1];
+}
