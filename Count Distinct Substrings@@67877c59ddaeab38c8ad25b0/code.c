24e9bfa4fb95fca45d13e69ab63a63a3c35cc844
Tanishque <tanishque1308@gmail.com>
2025-04-11 01:37:49 +0530

    Solution to Count Distinct Substrings

---
+++ Count Distinct Substrings@@67877c59ddaeab38c8ad25b0/code.c
@@ -0,0 +1,14 @@
+// Your code here...
+#include<stdio.h>
+#include<string.h>
+#include<maths.h>
+int main(){
+    char str[100];
+    scanf("%[^\n]",str);
+    for(i=0;str[i]!='\0';i++){
+        int x=sizeof(str)
+        printf("%d",pow(x,2));
+
+
+    }
+}
