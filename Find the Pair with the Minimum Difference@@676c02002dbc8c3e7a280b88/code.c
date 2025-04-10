c9c5c5ea1132ad6fd19ca5ad3e9010d0586ccdbf
Tanishque <tanishque1308@gmail.com>
2025-04-10 17:22:54 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -1,36 +1,47 @@
 // Your code here...
-#include<stdio.h>
-#include<string.h>
-int main(){
-    int n,i,j;
-    scanf("%d",&n);
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+    scanf("%d", &n);
+
+    // If only one element is present, print -1 and exit
+    if (n < 2) {
+        printf("-1\n");
+        return 0;
+    }
+
     int arr[n];
-    for(i=0;i<n;i++){
-        scanf("%d",&arr[i]);
-    }
-    int temp;
-    for(i=0;i<n;i++){
-        if(arr[i]>arr[i+1]){
-            temp=arr[i];
-            arr[i]=arr[i+1];
-            arr[i+1]=temp;
 
-    
+    // Input the array
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Sort using bubble sort
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
         }
     }
-    int fir,sec;
-    int small=arr[n-1]-arr[n-2];
-    for(i=0;i<n;i++){
-        if(arr[i]-arr[i+1]<small){
-            fir=arr[i];
-            sec=arr[i+1];
 
+    // Find the smallest difference between adjacent elements
+    int minDiff = arr[1] - arr[0];
+    int first = arr[0], second = arr[1];
+
+    for (i = 1; i < n - 1; i++) {
+        int diff = arr[i + 1] - arr[i];
+        if (diff < minDiff) {
+            minDiff = diff;
+            first = arr[i];
+            second = arr[i + 1];
         }
-
     }
-    printf("%d %d",fir,sec);
-    
-    
 
-
+    printf("%d %d\n", first, second);
+    return 0;
 }
