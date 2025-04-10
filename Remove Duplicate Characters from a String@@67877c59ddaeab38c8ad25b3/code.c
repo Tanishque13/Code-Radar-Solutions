6d69da0fbba1ce59a6ae40ce35fd7f15773e83de
Tanishque <tanishque1308@gmail.com>
2025-04-11 01:19:22 +0530

    Solution to Remove Duplicate Characters from a String

--- Remove Duplicate Characters from a String@@67877c59ddaeab38c8ad25b3/code.c
+++ Remove Duplicate Characters from a String@@67877c59ddaeab38c8ad25b3/code.c
@@ -1,19 +1,26 @@
 // Your code here...
-#include<stdio.h>
-#include<string.h>
-int main(){
+#include <stdio.h>
+#include <string.h>
+
+int main() {
     char str[100];
     int i;
-    scanf("%[^\n]",&str);
-    int freq[256]={0};
-    for(i=0;str[i]!='\0';i++){
+    int freq[256] = {0};
+
+    // Read input with spaces
+    scanf(" %[^\n]", str);
+
+    // Count frequency of each character
+    for (i = 0; str[i] != '\0'; i++) {
         freq[(int)str[i]]++;
     }
-    for(i=0;str[i]!='\0';i++){
-        if(freq[(int)str[i]]==1){
-            printf("%c",str[i]);
+
+    // Print characters that appear only once
+    for (i = 0; str[i] != '\0'; i++) {
+        if (freq[(int)str[i]] == 1) {
+            printf("%c", str[i]);
         }
     }
 
-
+    return 0;
 }
