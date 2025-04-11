59a698113f53e4f9975e6b28b75633e061ca5de8
Tanishque <tanishque1308@gmail.com>
2025-04-11 12:14:58 +0530

    Solution to Topper Tracker – Breaking Test Score Records

--- Topper Tracker – Breaking Test Score Records@@67de00956ac3fec9afb90e1a/code.c
+++ Topper Tracker – Breaking Test Score Records@@67de00956ac3fec9afb90e1a/code.c
@@ -4,34 +4,21 @@ int trackScores(int scores[],int n,int result[]){
     int high=scores[0];
     int highsco=0;
     int low=scores[0];
-    int lowsco=0;
-
+    inr lowsco=0;
     for(i=0;i<n;i++){
-        if(scores[i]>scores[0]){
-            high=scores[i];
-
-        }
-        for(i=0;i<n;i++){
-            if(scores[i]>high){
-                highsco++;
-            }
-        }
-        if(scores[i]<scores[0]){
-            low=scores[i];
-
-        }
-        for(i=0;i<n;i++){
-            if(scores[i]<low){
-                lowsco++;
-            }
-        }
+        if(scores[i]>high){
+            highsco++;
 
 
+        }
+        if(scores[i]<low){
+            lowsco++;
+
+
+        }
 
     }
     printf("%d %d",highsco,lowsco);
-    
-    
 
 
 }
