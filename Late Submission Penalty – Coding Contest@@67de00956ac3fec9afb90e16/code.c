7c04e45a257ee1c66df0a84e8f24a6e2497f617e
Tanishque <tanishque1308@gmail.com>
2025-04-11 13:37:45 +0530

    Solution to Late Submission Penalty – Coding Contest

--- Late Submission Penalty – Coding Contest@@67de00956ac3fec9afb90e16/code.c
+++ Late Submission Penalty – Coding Contest@@67de00956ac3fec9afb90e16/code.c
@@ -2,7 +2,7 @@
 int calculatePenalty(int sd,int sm,int sy,int dd,int dm,int dy){
     int penalty=0;
     if(sy>dy){
-        penalty=(sy-dy)*5000
+        penalty=(sy-dy)*5000;
     }
     printf("%d",penalty);
 
