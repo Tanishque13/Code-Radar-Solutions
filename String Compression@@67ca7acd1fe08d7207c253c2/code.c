1cbfb076889aeec9a8a6a87f551a11e02030cdd6
Tanishque <tanishque1308@gmail.com>
2025-04-11 12:31:22 +0530

    Solution to String Compression

---
+++ String Compression@@67ca7acd1fe08d7207c253c2/code.c
@@ -0,0 +1,24 @@
+// Your code here...
+compressString(char str[],char compressed[]){
+    int freq[256]={0};
+    for(i=0;str[i]!='\0';i++){
+        freq[(int)str[i]]++;
+
+    }
+    int count=0;
+    for(i=0;str[i]!='\0';i++){
+        if(freq[256]==1){
+            count=0;
+
+        }else{
+            count=1;
+        }
+    }
+    if(count){
+        printf("%c",str);
+    }
+    else{
+        printf("%c %d",str[0],freq);
+    }
+
+}
